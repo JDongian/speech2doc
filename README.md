38c2f9df44d8f011496eccdae80f7055a9651c0d
Joshua <jdong42@gmail.com>
2019-03-03 17:13:36 -0600

    fix

---
+++ README.md
@@ -0,0 +1,15 @@
+# Speech2Doc
+
+Convert an audio file to a transcript document. Just an example script, feel
+free to modify for specific needs.
+
+Currently produces docx, but a latex template is also provided for pdf
+generation.
+
+See https://www.rev.ai/ for more information. API key available with free
+trial.
+
+
+## License
+
+Apache. Free to use and modify.
--- test.py
+++ test.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+# Copyright 2019 Joshua Dong
+# 
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy of the
+# License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+"""Example to convert audio URI to polished transcript file.
+"""
 from argparse import ArgumentParser
 import re
 import os
@@ -51,14 +67,14 @@ def _main(args):
 
     wait_for(client, job)
 
-    transcript = client.get_transcript_json(job_id)
+    transcript = client.get_transcript_json(job.id)
 
     with open('transcript.json', 'w') as fp:
         json.dump(transcript, fp)
 
     render_md('transcript.json', 'transcript.md')
 
-    produce_docx('transcript.md', args.output)
+    produce_docx('transcript.md', args.o)
 
 
 def _main2(key, job_id):
